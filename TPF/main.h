a6b7a49e5b13c5166aa12afb469b41d7050e5a92
igonzalezb <igonzalez@itba.edu.ar>
2016-12-13 19:46:35 -0300

    gcc

--- TPF/main.h
+++ TPF/main.h
@@ -1,5 +1,7 @@
 //#define _RASPBERRY_PI_
 #define _ALLEGRO_PC_
+//RPI:
+//gcc libaudio.c main.c simon.c rpi_input.c rpi_output.c rpi_setup.c -I/usr/local/include -L/usr/local/lib -lSDL -o simon -lwiringPi
 
 
 
